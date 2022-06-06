3007495ee34d8448025e0fb60412d898251d003e
Your Name <you@example.com>
2022-06-06 16:32:57 +0530

    second commit

--- karatte/settings.py
+++ karatte/settings.py
@@ -130,6 +130,8 @@ STATICFILES_DIRS = (
 MEDIA_URL = '/media/'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 
+LOGIN_REDIRECT_URL='/admin/login/load_admin_home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- karatte_org/admin.py
+++ karatte_org/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+admin.site.register(hbgimg)
+admin.site.register(imagefolder)
+admin.site.register(images)
+admin.site.register(blackbelt_holders)
+
